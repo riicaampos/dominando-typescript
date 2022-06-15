88632bbad9eb19ba4273ee7a22730c56147f846f
Ricardo <ricardo.campos@myahmed.com>
2022-06-15 10:12:58 -0300

    subindo outras aulas

--- scripts/secao11/index.ts
+++ scripts/secao11/index.ts
@@ -1,4 +1,5 @@
-import { debug, log } from "./utils/decorators";
+import { debug, log, decoratorAttr, decoratorGetSet, decoratorParam } from "./utils/decorators";
+import "reflect-metadata"
 
 
 @debug
@@ -12,3 +13,53 @@ class SegundaClasse{
 }
 
 console.log(new SegundaClasse())
+
+
+
+class Animal{
+
+   @decoratorAttr
+   nome: string
+
+   constructor(nome : string){
+       this.nome = nome
+   }
+
+}
+
+const dog = new Animal("Trembo")
+
+console.log(dog.nome)
+
+class Login{
+
+ constructor(private _usuario: String, 
+             private _senha: string){}
+
+  @decoratorGetSet(true)
+  get usuario(){
+    return this._usuario
+  }           
+
+  @decoratorGetSet(false)
+  get senha(){
+    return this._senha
+  }
+
+}
+
+const login = new Login("ricampos","ricardo1234")
+
+for(let chave in login){
+    console.log(`chave ${chave}`)
+    // console.log(`chave ${login[chave]}`)
+}
+
+class TratarParametros{
+
+   metodo1(@decoratorParam mensagem : string){}
+   metodo2(@decoratorParam numero : number){}
+
+}
+
+new TratarParametros()
--- scripts/secao11/utils/decorators.ts
+++ scripts/secao11/utils/decorators.ts
@@ -9,3 +9,37 @@ export function log(constructor : any){
    }
 
 }
+
+
+export function decoratorAttr(target : unknown, nomePropriedade : string){
+
+     const novoNome = `_${nomePropriedade}`
+
+    Object.defineProperty(target, nomePropriedade,{
+        get(){
+          return this[novoNome].toUpperCase()
+        },
+        set(novoValor){
+          this[novoNome] = novoValor
+        }
+    })
+
+}
+
+export function decoratorGetSet(valor : boolean){
+
+  return function(target: unknown,
+                  propertyKey: string,
+                  descriptor: PropertyDescriptor){
+
+          descriptor.enumerable  = valor        
+
+  }
+}
+
+export function decoratorParam(target: any,
+                               key: string,
+                               propertyKey: number){
+
+      Reflect.getMetadata("design:paramtypes", target, key).map((item: any) => console.log(item))                         
+}
